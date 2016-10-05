5401bd749b56117b99b18ce5ad0762155908f152
Zeitlin1 <Zeitlin1@gmail.com>
2016-10-05 14:44:57 -0400

    Add files via upload
    add storyboard elements and view controller

---
+++ TableViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TableViewController.swift
+//  NavStuff
+//
+//  Created by James Campagno on 6/16/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let dirtyDancingSoundTrack: [String] = [
+        "(I've Had) The Time of My Life",
+        "Be My Baby",
+        "She's Like the Wind",
+        "Hungry Eyes",
+        "STAY",
+        "Yes",
+        "You Don't Own Me",
+        "Hey Baby",
+        "Overload",
+        "Love Is Strange",
+        "Where Are You Tonight?",
+    "In the Still of the Night"
+    ]
+
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dirtyDancingSoundTrack.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+ 
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DirtyCell", for: indexPath)
+        
+        let songTitle = dirtyDancingSoundTrack[indexPath.row]
+        cell.textLabel?.text = songTitle
+        
+        return cell
+        
+}
+}
